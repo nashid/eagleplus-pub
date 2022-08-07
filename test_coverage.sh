8d3cae2cc1661eb099e07b265e164b9d88837fc6
lin tan <10537693+lin-tan@users.noreply.github.com>
2022-08-06 23:21:25 -0400

    concurrency tests

--- test_coverage.sh
+++ test_coverage.sh
@@ -4,8 +4,11 @@
 # coverage run --parallel-mode --source . -m unittest discover . "test*.py"
 
 # pytest run pattern
-coverage run --source . -m pytest torchrec -v -s -W ignore::pytest.PytestCollectionWarning --continue-on-collection-errors -k 'not test_sharding_gloo_cw'
+# CUBLAS_WORKSPACE_CONFIG=:16:8 coverage run --source . --concurrency=multiprocessing -m pytest torchrec -v -s -W ignore::pytest.PytestCollectionWarning --continue-on-collection-errors -k 'not test_sharding_gloo_cw'
+# coverage run --source . --concurrency=multiprocessing -m pytest torchrec -v -s -W ignore::pytest.PytestCollectionWarning --continue-on-collection-errors -k 'not test_sharding_gloo_cw'
+coverage run -m pytest torchrec -v -s -W ignore::pytest.PytestCollectionWarning --continue-on-collection-errors -k 'not test_sharding_gloo_cw'
 
+coverage combine
 
 # coverage report
-coverage html -i --omit "*/test_*.py","*/*_test.py" --include="torchrec/*"
+coverage html -i --skip-empty --omit "*/tests/test_*.py","*/test_utils/test_*.py","*_tests.py" --include="torchrec/*"
